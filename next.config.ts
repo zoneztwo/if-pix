e567a60b457e77e36caedcc06ae218ab2e07c71f
zoneztwo <isko.jpg@gmail.com>
2026-02-23 20:12:05 +0600

    fix fidx fix fixxxx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  // Standalone modunda gereksiz dosyalar temizlenir ve sunucu daha hızlı başlar.
+  experimental: {
+    // Turbopack ayarları veya diğer deneysel özellikler buraya eklenebilir
+  }
 };
 
 export default nextConfig;
