955a1fd8eebdb66ab6c614454c09a3ec5382e476
zoneztwo <isko.jpg@gmail.com>
2026-02-23 19:40:36 +0600

    fix fix fix fixx fix

--- src/components/pages/HakkimizdaClient.tsx
+++ src/components/pages/HakkimizdaClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import PixelBackground from '@/components/PixelBackground';
